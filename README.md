98411f9ca5073eae8a1d7ee9b4b0f23969c2e775
Michelle Wu <michelle.wu@networld.hk>
2023-10-20 16:08:33 -0400

    add dummy code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Microservice-1
 Microservice 1 deloying with EC2
+
+This will be our first microservice, and we will deploy it to Google App Engine
---
+++ main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI, Response
+
+app=FastAPI()
+
+@app.get("/")
+async def base():
+    the_message = f"This is the AWS EC2 feature. To be continued..."
+    rsp = Response(content=the_message, media_type="text/plain")
+    return rsp
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8012)
