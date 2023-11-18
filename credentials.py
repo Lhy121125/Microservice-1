6d51c5c7a771990665f5dcb0150632b0a24ca213
Michelle Wu <michelle.wu@networld.hk>
2023-11-17 19:21:30 -0500

    add credentials file

---
+++ credentials.py
@@ -0,0 +1,7 @@
+
+db_credentials = {
+    'rds_endpoint': 'your-rds-endpoint.amazonaws.com',
+    'db_name': 'your_database_name',
+    'db_user': 'your_username',
+    'db_password': 'your_password',
+}
